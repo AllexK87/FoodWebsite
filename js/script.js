58ff6de7ffcea051f1342de6dddf2430b4a6bdf1
AllexK87 <allex.k87@ya.ru>
2021-09-19 19:05:40 +0300

    serch + home-slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Menu bars
 let menu = document.querySelector('#menu-bars');
 let nav = document.querySelector('.header__menu');
 
@@ -5,3 +6,29 @@ menu.onclick = () =>{
     menu.classList.toggle('ic-times');
     nav.classList.toggle('menu-open')
 }
+
+// Search 
+// Open
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+// Close
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+// Swiper
+const swiper = new Swiper ('.home-slider', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop:true,
+})
